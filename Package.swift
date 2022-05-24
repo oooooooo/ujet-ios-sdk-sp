75fb59a6c644d6f388f406d716dcb1d683316ee3
jaesung <jaesung@ujet.cx>
2022-05-25 00:09:40 +0900

    Update 0.46.0

--- Package.swift
+++ Package.swift
@@ -3,32 +3,69 @@
 import PackageDescription
 
 let package = Package(
-    name: "UJET",
-    platforms: [
-        .iOS(.v12)
-    ],
-    products: [
-        .library(
-            name: "UJET",
-            targets: ["WrapperUJET"])
-    ],
-    dependencies: [
-         .package(url: "https://github.com/twilio/conversations-ios", from: "2.2.1"),
-         .package(url: "https://github.com/twilio/twilio-voice-ios", from: "6.3.1")
-    ],
-    targets: [
-        .binaryTarget(
-            name: "UJET",
-            url: "https://sdk.ujet.co/ios/0.45.1/UJET.xcframework.zip",
-            checksum: "d627d03cbc79e9d276752e2e35b88f81191d3bdebe07d062b9ac7e5bc8dc67ac"),
+  name: "UJET",
+  platforms: [
+    .iOS(.v12)
+  ],
+  products: [
+    .library(
+      name: "UJET",
+      targets: ["WrapperUJET"]),
+    .library(
+      name: "UJETCobrowse",
+      targets: ["WrapperUJETCobrowse"]),
+    .library(
+      name: "UJETCobrowseExtension",
+      targets: ["WrapperUJETCobrowseExtension"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/twilio/conversations-ios", .exact("2.2.2")),
+    .package(url: "https://github.com/twilio/twilio-voice-ios", .exact("6.3.1")),
+    .package(url: "https://github.com/cobrowseio/cobrowse-sdk-ios-binary", .exact("2.18.0"))
+  ],
+  targets: [
+    .binaryTarget(
+      name: "UJETFoundation",
+      url: "https://sdk.ujet.co/ios/0.46.0/UJETFoundation.xcframework.zip",
+      checksum: "ab9ebefae0ac218bfbf4e5554e5c56bf5d6a0f999c3429426c599e1e76c0cbf6"
+    ),
 
-        .target(
-            name: "WrapperUJET",
-            dependencies: [
-                "UJET", 
-                .product(name: "TwilioConversationsClient", package: "conversations-ios"), 
-                .product(name: "TwilioVoice", package: "twilio-voice-ios")
-            ]
-        )
-    ]
+    .binaryTarget(
+      name: "UJET",
+      url: "https://sdk.ujet.co/ios/0.46.0/UJET.xcframework.zip",
+      checksum: "4ece575e6aa50d171ab4e10f78c768bf59214faf8671b95327cc024fdbab9254"
+    ),
+
+    .binaryTarget(
+      name: "UJETCobrowse",
+      url: "https://sdk.ujet.co/ios/0.46.0/UJETCobrowse.xcframework.zip",
+      checksum: "cda85a4d55d5758e27561685c49a4c3fd8d34d16bd507d18bde712a2a11a9f3f"
+    ),
+
+    .target(
+      name: "WrapperUJET",
+      dependencies: [
+        "UJET",
+        "UJETFoundation",
+        .product(name: "TwilioConversationsClient", package: "conversations-ios"), 
+        .product(name: "TwilioVoice", package: "twilio-voice-ios")
+      ]
+    ),
+
+    .target(
+      name: "WrapperUJETCobrowse",
+      dependencies: [
+        "UJETFoundation",
+        "UJETCobrowse",
+        .product(name: "CobrowseIO", package: "cobrowse-sdk-ios-binary")
+      ]
+    ),
+
+    .target(
+      name: "WrapperUJETCobrowseExtension",
+      dependencies: [
+        .product(name: "CobrowseIOAppExtension", package: "cobrowse-sdk-ios-binary")
+      ]
+    )
+  ]
 )
