634390bdba94a2797ed4dd3c5739a3b43ddabbba
runner <runner@Mac-1682014918567.local>
2023-04-20 19:12:54 +0000

    Update 2.0.3

--- Package.swift
+++ Package.swift
@@ -26,31 +26,31 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "UJETFoundationKit",
-      url: "https://sdk.ujet.co/ios/2.0.2/UJETFoundationKit.xcframework.zip",
-      checksum: "b61480e42ea98dfc882871920d67edd80da21ecd43b9d785598e97e97229c9ac"
+      url: "https://sdk.ujet.co/ios/2.0.3/UJETFoundationKit.xcframework.zip",
+      checksum: "ac1e27058a5447b01b59dd9d7f987950682518a08ace743d0d6f0432e5eec4a0"
     ),
 
     .binaryTarget(
       name: "UJETKit",
-      url: "https://sdk.ujet.co/ios/2.0.2/UJETKit.xcframework.zip",
-      checksum: "383b48689c074cb3f3722009b8a78d590d448a52e6e50a39e5b0e649946c4f6c"
+      url: "https://sdk.ujet.co/ios/2.0.3/UJETKit.xcframework.zip",
+      checksum: "306e73a9758d44c8295de74aab37e36af8676900cc53fd499d10ea5b51bcd901"
     ),
 
     .binaryTarget(
       name: "UJETCobrowseKit",
-      url: "https://sdk.ujet.co/ios/2.0.2/UJETCobrowseKit.xcframework.zip",
-      checksum: "28a578b9dce801f88824697c2495fa36c1b4d30259435e89bd95e77642f6d1c7"
+      url: "https://sdk.ujet.co/ios/2.0.3/UJETCobrowseKit.xcframework.zip",
+      checksum: "a678f4c8275cb53d0dc691b2ffd9be9ca57fa0ec9379c3f8cc2bd06ee6bb4d2a"
     ),
 
     .binaryTarget(
       name: "UJETChatRedKit",
-      url: "https://sdk.ujet.co/ios/2.0.2/UJETChatRedKit.xcframework.zip",
-      checksum: "3d350e598a5448a20434692a76779168a548b18206b744cfaef9b6301f99f84b"
+      url: "https://sdk.ujet.co/ios/2.0.3/UJETChatRedKit.xcframework.zip",
+      checksum: "0a70dcb301a9c750fe4b345fa73052c0ec0684d86b9ab7496a6e48a851e7d88f"
     ),
     .binaryTarget(
       name: "UJETCallRedKit",
-      url: "https://sdk.ujet.co/ios/2.0.2/UJETCallRedKit.xcframework.zip",
-      checksum: "45153cec0bc684f0f6da7974609079000afce475d663bbf29f4d0c6612ab9ab9"
+      url: "https://sdk.ujet.co/ios/2.0.3/UJETCallRedKit.xcframework.zip",
+      checksum: "7b4f1ffa508cf812ef6d89d9bdc2df41029e0a76d24e326689f8c486747db065"
     ),
 
     .target(
