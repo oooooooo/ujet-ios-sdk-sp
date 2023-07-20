2915931007902a02c8160633bcaa261e8f2c08d4
runner <runner@Mac-1689873541864.local>
2023-07-20 18:10:11 +0000

    Update 2.1.1

--- Package.swift
+++ Package.swift
@@ -26,31 +26,31 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "UJETFoundationKit",
-      url: "https://sdk.ujet.co/ios/2.1.0/UJETFoundationKit.xcframework.zip",
-      checksum: "b0853f2e75c97d3d7b79e26ede33fbbf995efaa271e3ca09e178e4a8b961eb12"
+      url: "https://sdk.ujet.co/ios/2.1.1/UJETFoundationKit.xcframework.zip",
+      checksum: "d582acebc506bfcd7f9da4ba4b972264e013833036d3aa799bb5d89f5cf0479f"
     ),
 
     .binaryTarget(
       name: "UJETKit",
-      url: "https://sdk.ujet.co/ios/2.1.0/UJETKit.xcframework.zip",
-      checksum: "cf4ccf6c0a38febe0f31284e0d3d8143db0466d0372f30fea6e3462e47c937da"
+      url: "https://sdk.ujet.co/ios/2.1.1/UJETKit.xcframework.zip",
+      checksum: "8bc04e3990de5ff08f008c7ad0d53aa67350125f3ac520475c9eabcc38c141f1"
     ),
 
     .binaryTarget(
       name: "UJETCobrowseKit",
-      url: "https://sdk.ujet.co/ios/2.1.0/UJETCobrowseKit.xcframework.zip",
-      checksum: "fdf64e008ff70f4bb802428c2d8f7e8180717b20bd950f1ee6d1ae636bb7aa40"
+      url: "https://sdk.ujet.co/ios/2.1.1/UJETCobrowseKit.xcframework.zip",
+      checksum: "7556c745d2ca46bb6817990debfa7ebf35e417015bc1dd13dddc71eb765df5ce"
     ),
 
     .binaryTarget(
       name: "UJETChatRedKit",
-      url: "https://sdk.ujet.co/ios/2.1.0/UJETChatRedKit.xcframework.zip",
-      checksum: "e5a1f982ac98e7336274eaf2636bce10234d9968a143b5dc9280535b2b70e961"
+      url: "https://sdk.ujet.co/ios/2.1.1/UJETChatRedKit.xcframework.zip",
+      checksum: "b42b1056e7f650e12130ac81f8de18c9a5f67d1365a19b7c438657254fd685ff"
     ),
     .binaryTarget(
       name: "UJETCallRedKit",
-      url: "https://sdk.ujet.co/ios/2.1.0/UJETCallRedKit.xcframework.zip",
-      checksum: "7b23cf563b835364c78dffae8bb057649b8f6101277c6207c0ee18ce03942d51"
+      url: "https://sdk.ujet.co/ios/2.1.1/UJETCallRedKit.xcframework.zip",
+      checksum: "659b2a391a96672fcbca9c98a15d96f6578904c07553e5118d01a87d03b8564f"
     ),
 
     .target(
