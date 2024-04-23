9b2d0f2e39b21a1980607ab4e86cf96a1bbb156d
Anka <runner@Mac-1713890519462.local>
2024-04-23 20:05:07 +0000

    Update 2.6.0

--- Package.swift
+++ Package.swift
@@ -27,30 +27,30 @@ let package = Package(
     .binaryTarget(
       name: "UJETFoundationKit",
       url: "https://sdk.ujet.co/ios/2.6.0/UJETFoundationKit.xcframework.zip",
-      checksum: "4b33ce47efe413fd2ba6477d9f3f46192019241305995aa7a3054d4685ab97fb"
+      checksum: "0965e50d22d94883d3d11eef8485bdcdd77b8238050dc5b9939ddcfc86787956"
     ),
 
     .binaryTarget(
       name: "UJETKit",
       url: "https://sdk.ujet.co/ios/2.6.0/UJETKit.xcframework.zip",
-      checksum: "27ab4acbc34bc50a8768e829fcc945ae00972b52eef8673cd15bc4535c4caf6f"
+      checksum: "361df5f77c3f6aa700b9e838ebc5403930d2a63c4bf8f7cb1742b23daf915319"
     ),
 
     .binaryTarget(
       name: "UJETCobrowseKit",
       url: "https://sdk.ujet.co/ios/2.6.0/UJETCobrowseKit.xcframework.zip",
-      checksum: "3bce7ee5bddd13a1463ee69becd8573a10d958beaea5ac0e53ccc87bb0271849"
+      checksum: "e72d3d2de67e060bccb784edc271f0017e8dba75719ff8322092cb2487313e4b"
     ),
 
     .binaryTarget(
       name: "UJETChatRedKit",
       url: "https://sdk.ujet.co/ios/2.6.0/UJETChatRedKit.xcframework.zip",
-      checksum: "dff0c798cd4727d8a06af978e867b6a035100a4cdf2369b78cc985de5788284a"
+      checksum: "5101aca357b6e5c07685fa1a98d4d1c5273ee2ecfaeb32e17732e591fa298465"
     ),
     .binaryTarget(
       name: "UJETCallRedKit",
       url: "https://sdk.ujet.co/ios/2.6.0/UJETCallRedKit.xcframework.zip",
-      checksum: "29ad8e1c08294a974a387345732b1dbb02c831f4ce3b408a4114dd84cba854e4"
+      checksum: "d887e0fb7fe472fdb8a575cb143bb29d77301e87ebe125ecded4c423a6d74231"
     ),
 
     .target(
